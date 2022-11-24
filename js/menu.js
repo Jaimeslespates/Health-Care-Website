b5522048ea15b46cb6df38902c44213a869c572b
_Jaimeslespates <ballivetmathys@gmail.com>
2022-11-24 15:53:29 +0100

    Menu Hamburger fait + quelques finitions

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,10 @@
-const menu = document.querySelector(".menu")
-const header = document.querySelector(".header")
-const closeMenu = document.querySelector(".close")
+const menu = document.querySelector(".header-menu")
+const header = document.querySelector("header")
+const closeMenu = document.querySelector(".header-menu-close")
 
 menu.addEventListener('click', ()=> {
     header.classList.toggle("mobile-active")
-})
+});
 
 closeMenu.addEventListener('click', ()=> {
     header.classList.toggle("mobile-active")
